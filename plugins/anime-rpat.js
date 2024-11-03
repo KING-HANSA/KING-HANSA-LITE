0972d75a6b06429343aadcf38dac13b6a764204e
KING-HANSA <hansadewmina2008@gmail.com>
2024-11-03 15:10:50 +0530

    Update anime-rpat.js

--- plugins/anime-rpat.js
+++ plugins/anime-rpat.js
@@ -5,8 +5,8 @@ const { cmd, commands } = require('../command');
 cmd({
 pattern: "pat",
 desc: "Fetch a random anime pat.",
-category: "fun",
-react: "👧",
+category: "misc",
+react: "🍒",
 filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
