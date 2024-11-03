cb064715df4fee416eadab53642dd793561e0803
KING-HANSA <hansadewmina2008@gmail.com>
2024-11-03 15:10:23 +0530

    Update anime-slap.js

--- plugins/anime-slap.js
+++ plugins/anime-slap.js
@@ -5,8 +5,8 @@ const { cmd, commands } = require('../command');
 cmd({
 pattern: "slap",
 desc: "Fetch a random slap",
-category: "fun",
-react: "👧",
+category: "misc",
+react: "🍒",
 filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
