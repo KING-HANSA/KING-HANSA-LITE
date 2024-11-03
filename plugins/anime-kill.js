9f818873e9d819f3b0e1a9bdb64aeed94be1bd8c
KING-HANSA <hansadewmina2008@gmail.com>
2024-11-03 14:54:51 +0530

    Create anime-kill.js

---
+++ plugins/anime-kill.js
@@ -0,0 +1,23 @@
+
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+pattern: "kill",
+desc: "Fetch a random anime kill image.",
+category: "fun",
+react: "👧",
+filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+try {
+const apiUrl = `https://api.waifu.pics/sfw/kill`;
+const response = await axios.get(apiUrl);
+const data = response.data;
+
+await conn.sendMessage(from, { image: { url: data.url }, caption: '🍁 *Random Anime kill Image* 🍓\n> KING-HANSA-LITE' }, { quoted: mek });
+} catch (e) {
+console.log(e);
+reply(`Error fetching anime kill image: ${e.message}`);
+}
+});
