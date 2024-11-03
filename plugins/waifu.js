b853a4f778feac1c8e1dbf14960b5bae97e43108
KING-HANSA <hansadewmina2008@gmail.com>
2024-11-03 11:56:18 +0530

    Create waifu.js

---
+++ plugins/waifu.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { cmd, commands } = require('../command');
+
+cmd({
+pattern: "waifu",
+desc: "Fetch a random waifu girl image.",
+category: "fun",
+react: "🍓",
+filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+try {
+const apiUrl = `https://api.waifu.pics/nsfw/waifu`;
+const response = await axios.get(apiUrl);
+const data = response.data;
+
+await conn.sendMessage(from, { image: { url: data.url }, caption: '🍁 *Random waifu Girl Image* 🍓\n> KING-HANSA-LITE' }, { quoted: mek });
+} catch (e) {
+console.log(e);
+reply(`Error fetching anime girl image: ${e.message}`);
+}
+});
