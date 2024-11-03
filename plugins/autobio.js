149588e9b146abc05d4f2719c8007e28f1c860f9
KING-HANSA <hansadewmina2008@gmail.com>
2024-11-03 12:56:18 +0530

    Update autobio.js

--- plugins/autobio.js
+++ plugins/autobio.js
@@ -16,6 +16,7 @@ World best and powerfull whatsapp user bot in Sri lanka*/
 
 const { cmd } = require('../command');
 const config = require('../config');
+const os = require("os");
 
 // AutoBIO feature variables
 let autoBioInterval;
@@ -49,7 +50,7 @@ function startAutoBio(conn) {
     // Set a new interval to update the bio every minute (or any preferred time)
     autoBioInterval = setInterval(async () => {
         const time = new Date().toLocaleTimeString();  // Get the current time
-        const bioText = `💚24/7 𝗢𝗡𝗟𝗜𝗡𝗘 𝗕𝗢𝗧 𝗕𝗬 𝗛𝗔𝗡𝗦𝗔 𝗗𝗘𝗪𝗠𝗜𝗡𝗔 💛`;  // Set the bio text with time
+        const bioText = `💚24/7 𝗢𝗡𝗟𝗜𝗡𝗘 𝗕𝗢𝗧 𝗕𝗬 𝗛𝗔𝗡𝗦𝗔 𝗗𝗘𝗪𝗠𝗜𝗡𝗔 💛 ${runtime(process.uptime())}`;  // Set the bio text with time
         await conn.updateProfileStatus(bioText);  // Update the bot's bio
     }, 60 * 1000);  // 1 minute interval
 }
